390aab97e71d648fee039fde036a16f414e1bb0f
Jordan Chacón Villacís <jchaconvillacis@gmail.com>
2026-03-14 12:02:00 -0500

    Fixing RedistributionModule.tsx by completing JSX fragments and closing all tags

---
+++ RedistributionModule.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RedistributionModule = () => {
+    return (
+        <div>
+            <h1>Redistribution Module</h1>
+            {/* Your components go here */}
+        </div>
+    );
+};
+
+export default RedistributionModule;
